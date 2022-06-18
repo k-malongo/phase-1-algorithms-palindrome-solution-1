ad1a7d3563f8a7c9a39ba51e871c6c5b5b8347c9
Kelvin <vokemalosh32@gmail.com>
2022-06-18 18:40:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var len = Math.floor(word.length / 2);
+  for (var i = 0; i < len; i++)
+    if (word[i] !== word[word.length - i - 1])
+      return false;
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+  I have a function isPalindrome which splits the word in two
+  Then it checks whether the first letter and the last letter of the string equate
+  It returns true if they equal.else it returns false.
 */
 
 // You can run `node index.js` to view these console logs
